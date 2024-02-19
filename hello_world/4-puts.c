896b758e6d80d4ac4511b3e90cedbaa38b5c1d6b
essioui <salahessioui@gmail.com>
2024-02-19 13:41:44 +0000

    puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+/*write bib of progr c*/
 int main(void)
 {
+/*Print one line with function puts*/
 puts("\"Programming is like building a multilingual puzzle");
 return (0);
+/*end function main*/
 }

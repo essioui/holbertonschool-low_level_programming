e224a6d073019ef757b1c71e6574e266d0cdf210
essioui <salahessioui@gmail.com>
2024-02-20 10:41:01 +0000

    positive

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,13 +11,21 @@ int main(void)
 	if (n > 0)
 	{
 		printf("%d is positive", n);
-} else if (n < 0)
-	{
+        }
+        else
+	  {	
+		if (n < 0)
+	    {  
 		printf("%d is negative", n);
-	else
-	{
+	    }
+	        else { 
+		      if (n == 0)
+	             {
 			printf("%d is zero", n);
-		}
-	}
-	return (0);
+	             }
+		      else {
+			return (0);
+	                }
+	             }
+	  }
 }

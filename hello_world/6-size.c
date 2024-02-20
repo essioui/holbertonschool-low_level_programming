a27b169430adc4592702e504b7fa3dbad66a7f37
essioui <salahessioui@gmail.com>
2024-02-20 09:03:14 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,9 +1,16 @@
 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	printf("size of a char: %c\n", sizeof(char));
-	printf("size of a int: %d\n", sizeof(short int));
-	printf("size of a long int: %ld\n", sizeof(long int));
-	printf("size of a long long int: %lld\n", sizeof(long double));
-	printf("size of a float: %2f\n", sizeof(float));
+printf("size of a char: %c bytes \n", sizeof(char));
+printf("size of a int: %d bytes \n", sizeof(short int));
+printf("size of a long int: %ld bytes \n", sizeof(long int));
+printf("size of a long long int: %lld bytes \n", sizeof(long double));
+printf("size of a float: %2f bytes \n", sizeof(float));
 }

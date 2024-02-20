f6e63ca56863444b17fa7e2117e8d788158445dd
essioui <salahessioui@gmail.com>
2024-02-20 08:50:18 +0000

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(void)
 {
-	printf("size of a char: %c", 4 );
-	printf("size of a int: %hd", 2 );
+	printf("size of a char: %c", 1 );
+	printf("size of a int: %hd", 4 );
 	printf("size of a long int: %ld", 4 );
 	printf("size of a long long int: %lld", 8 );
 	printf("size of a float: %f", 4 );

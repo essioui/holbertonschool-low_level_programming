74e562a00f1efa2fa3f264aa1070b3f14bf9cec4
essioui <salahessioui@gmail.com>
2024-02-20 12:34:46 +0000

    pos

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,15 +14,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is %s\n", n, "is positive");
+		printf("%d is positive");
 	}
 	else if (n == 0)
 	{
-		printf("%d is %s\n", n, "is zero");
+		printf("%d is zero");
 	}
 	else
 	{
-		printf("%d is %s\n", n,"is negative");
+		printf("%d is negative");
 	}
 	return (0);
 }

37bf4bfa155b32388fa460b6868090431f47419e
essioui <salahessioui@gmail.com>
2024-02-21 09:30:12 +0000

    last_digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ else if (last_digit == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, last_digit);
 }
-else 
+else
 {
 printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_digit);
 }

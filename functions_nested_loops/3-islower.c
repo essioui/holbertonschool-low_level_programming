236a4dfca22a7699c900f5339fd39484475f2e52
essioui <salahessioui@gmail.com>
2024-02-22 13:35:43 +0000

    islower

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <ctype.h>
+/**
+ * print_alphabet - use void_alphabet
+ *
+ * Return
+ */
+int _islower(int c)
+{
+if(c >= 'a' && c <= 'z')
+{
+return(1);
+}
+else
+{
+return(0);
+}
+}

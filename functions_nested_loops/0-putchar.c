be702b57995b4a9180bed41f6dc523d93ab2df62
essioui <salahessioui@gmail.com>
2024-02-22 10:41:06 +0000

    create

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,7 @@
 #include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - writes the character c to stdout
  *
- * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
 int main(void)

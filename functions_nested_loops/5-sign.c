f3dba56df494ca958e18f86b03a08405ed5f5830
essioui <salahessioui@gmail.com>
2024-02-23 10:45:23 +0000

    sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -8,10 +8,10 @@ int print_sign(int n)
 {
 if (n < 0)
 {
-_putchar('+');
-return (1);
+_putchar('-');
+return (-1);
 }
-else 
+else
 if (n == 0)
 {
 _putchar('0');
@@ -19,7 +19,7 @@ return (0);
 }
 else
 {
-_putchar('-');
-return (-1);
+_putchar('+');
+return (1);
 }
 }

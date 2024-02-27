d1f0ac3d1ad813a87f1f45ecb96a2f747d61d6b9
essioui <salahessioui@gmail.com>
2024-02-27 09:12:46 +0000

    print_diagonal

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_diagonal - function print n space than \
+ * @n: entier
+ */
+void print_diagonal(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0; i <= n; i++)
+_putchar(' ');
+_putchar('\\');
+}
+_putchar('\n');
+}

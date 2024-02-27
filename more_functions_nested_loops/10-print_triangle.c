890ca2deb28e2a84bf0197627896fd5a72a7a816
essioui <salahessioui@gmail.com>
2024-02-27 12:12:59 +0000

    triangle

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -5,24 +5,24 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for(i = 1; i <= size; i++)
-		{
-			for(j = 1; j <= size -i; j++)
-			{
-				_putchar(' ');
-			}
-			for (j = size - i + 1; j <= size; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+int i, j;
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 1; j <= size - i; j++)
+{
+_putchar(' ');
+}
+for (j = size - i + 1; j <= size; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }

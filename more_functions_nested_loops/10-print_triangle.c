4a07e03863afed8f700deb8238523d9c7211c3b7
essioui <salahessioui@gmail.com>
2024-02-27 12:08:46 +0000

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_triangle - function for typing triangle with #
+ * @size: entier
+ */
+void print_triangle(int size)
+{
+	int i, j;
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for(i = 1; i <= size; i++)
+		{
+			for(j = 1; j <= size -i; j++)
+			{
+				_putchar(' ');
+			}
+			for (j = size - i + 1; j <= size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}

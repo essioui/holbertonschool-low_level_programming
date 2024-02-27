7c1179844dad8305c3b992f89d84d7e0f99f783f
essioui <salahessioui@gmail.com>
2024-02-27 09:25:08 +0000

    print_diagonal

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -5,12 +5,15 @@
  */
 void print_diagonal(int n)
 {
-int i;
+int i, j;
 if (n > 0)
 {
 for (i = 1; i <= n; i++)
+{
+for (j = 1; j <= i - 1; j++) 
 _putchar(' ');
 _putchar('\\');
+}
 }
 _putchar('\n');
 }

2656499930be9e7de864bb3d01d5d72b95e6dd8c
essioui <salahessioui@gmail.com>
2024-02-28 13:43:34 +0000

    sauf

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -5,9 +5,10 @@
  */
 void puts2(char *str)
 {
-	int a;
-	for (a = 0; str[a] != '\0'; a += 2)
-	{
-		_putchar(str[a]);
-	}
+int a;
+for (a = 0; str[a] != '\0'; a += 2)
+{
+_putchar(str[a]);
+}
+_putchar('\n');
 }

cae2b81fa45edbfea2c5ee4506264994170128d9
essioui <salahessioui@gmail.com>
2024-02-29 11:58:21 +0100

    puts_second_half

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -1,17 +1,21 @@
 #include "main.h"
 /**
+ * puts_half - fuction affiche the second half of strinf
+ * @str: pointer
 */
 void puts_half(char *str)
 {
     int i, j, length;
-    int n, b;
+    int b, n;
     i = 0;
     for (j = 0; str[j] != '\0'; j++)
     {
         i++;
     }
-    length = i;
+    length= i;
+
     n = (length - 1) / 2;
+    
 
     for (b = n + 1; b < length; b++)
     {

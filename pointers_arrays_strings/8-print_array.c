2055fa7038eea041c2fad7cac30b4cc633326bd5
essioui <salahessioui@gmail.com>
2024-02-29 13:42:41 +0100

    puts_half

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -6,14 +6,14 @@
 void print_array(int *a, int n)
 {
 int i;
-for (i = 0; i < n ; i++)
+	
+for (i = 0; i < n; i++)
 {
+printf("%d", a[i]);
 if (i != n - 1)
 {
-printf(" %d;", a[i]);
+printf(", ");
 }
-else
-printf(" %d;", a[i]);
 }
-printf("\n");
+putchar('\n');
 }

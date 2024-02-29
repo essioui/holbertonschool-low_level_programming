c2a3afe9e8803796876a366e92a66fac2a01c126
essioui <salahessioui@gmail.com>
2024-02-29 11:49:14 +0100

    two_files

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,10 +8,10 @@ void puts2(char *str)
 int a;
 for (a = 0; str[a] != '\0'; a++)
 {
-    if (a % 2 != 0)
-    {
-        continue;
-    }
+if (a % 2 != 0)
+{
+continue;
+}
 _putchar(str[a]);
 }
 _putchar('\n');

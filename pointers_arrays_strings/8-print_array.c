55c88cfb340f550991fd116dbe3d65a797874cb2
essioui <salahessioui@gmail.com>
2024-02-29 13:46:17 +0100

    puts_half

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -1,12 +1,13 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * 
+ * print_array - function print
+ * @a: pointer
+ * @n: number of
 */
 void print_array(int *a, int n)
 {
-int i;
-	
+int i;	
 for (i = 0; i < n; i++)
 {
 printf("%d", a[i]);

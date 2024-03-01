13f06c4d13e6bdd0a72600a5c45051e0bf62d020
essioui <salahessioui@gmail.com>
2024-03-01 09:44:56 +0100

    atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,6 +2,7 @@
 /**
  * _toi - function transfer ASCII to integer
  * @s: pointer
+ * Return: return integer
 */
 int _atoi(char *s)
 {
@@ -10,25 +11,22 @@ int j = 0;
 int n = 0;
 for (; *s != '\0'; s++)
 {
-    if (*s == '-')
-    i *= -1;
-    else
-    if (*s >= '0' && *s <= '9')
-    break;    
+if (*s == '-')
+i *= -1;
+else
+if (*s >= '0' && *s <= '9')
+break;
 }
-
 while (*s >= '0' && *s <= '9')
 {
-    j *= 10;
-    j += *s - '0';
-    s++;
+j *= 10;
+j += *s - 48; // 48 en code ASCII equivaut '0'
+s++;
 }
 if (i == -1)
 n = -j;
 else
 n = j;
-
 return(n);
-
 }
 

48cb7f5799d221edb99a1741b43c11035bacfc81
essioui <salahessioui@gmail.com>
2024-03-01 11:11:52 +0100

    cat to strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -8,16 +8,16 @@
 */
 char *_strncat(char *dest, char *src, int n)
 {
-    int i , j;
-    i = 0;
-    while (dest[i] != '\0')
-    {
-        i++;
-    }
-        for (j = 0; j < n && src[j] != '\0'; j++, i++)
-        {
-            dest[i] = src[j];
-        }
+int i , j;
+i = 0;
+while (dest[i] != '\0')
+{
+i++;
+}
+for (j = 0; j < n && src[j] != '\0'; j++, i++)
+{
+dest[i] = src[j];
+}
 dest[i] = '\0';
 return (dest);       
 }

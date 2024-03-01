481b972807197e5dd021881000c50940e4df6db8
essioui <salahessioui@gmail.com>
2024-03-01 09:36:45 +0100

    atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * _toi - function transfer ASCII to integer
+ * @s: pointer
+*/
+int _atoi(char *s)
+{
+int i = 1;
+int j = 0;
+int n = 0;
+for (; *s != '\0'; s++)
+{
+    if (*s == '-')
+    i *= -1;
+    else
+    if (*s >= '0' && *s <= '9')
+    break;    
+}
+
+while (*s >= '0' && *s <= '9')
+{
+    j *= 10;
+    j += *s - '0';
+    s++;
+}
+if (i == -1)
+n = -j;
+else
+n = j;
+
+return(n);
+
+}
+

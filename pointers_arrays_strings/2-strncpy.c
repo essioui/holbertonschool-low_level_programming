ecb9fc05eb180a6318418db145130c8dab0a4d8b
essioui <salahessioui@gmail.com>
2024-03-01 13:03:03 +0100

    copies_strings

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strncpy - function for copies string
+ * @dest: string
+ * @src: string
+ * @n: integer
+ * Return: dest
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; src[i] != '\0' && i < n; i++)
+		dest[i]	= src[i];
+	while (i < n)
+		dest[i++] = '\0';
+	return (dest);
+}

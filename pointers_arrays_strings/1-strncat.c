c317e86a644883add438739acfe2632dab173970
essioui <salahessioui@gmail.com>
2024-03-01 11:11:00 +0100

    cat to strings

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -1,6 +1,10 @@
 #include "main.h"
 /**
- * 
+ * _strncat - function for copy string to other
+ * @dest: strig
+ * @src: string
+ * @n: integer
+ * Return: dest
 */
 char *_strncat(char *dest, char *src, int n)
 {

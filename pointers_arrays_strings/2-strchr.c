3c8a7e5cf9e00e11f223c5a44c08f83a9257d288
essioui <salahessioui@gmail.com>
2024-03-04 12:57:59 +0100

    strch

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -7,9 +7,9 @@ char *_strchr(char *s, char c)
 unsigned int i;
 while (s[i])
 {
-    if (s[i] == c)
-    break;
-    i++;
+if (s[i] == c)
+break;
+i++;
 }
-return ((s[i] || c == '\0') ? (s +i) : "nil");
+return ((s[i] || c == '\0') ? (s + i) : "nil");
 }

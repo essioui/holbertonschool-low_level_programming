90fbfbceebeba963ca0caf8063b4d43f5764c78a
essioui <salahessioui@gmail.com>
2024-03-04 13:42:26 +0100

    strch

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
- * *_strchr - locates a character in a string
+ * _strchr - locates a character in a string
  * @s: string
  * @c: charctere
  * Return: s or nil
 */
 char *_strchr(char *s, char c)
 {
-unsigned int i;
-while (s[i])
-{
-if (s[i] == c)
-break;
-i++;
-}
-return ((s[i] || c == '\0') ? s + i : "nil");
+	int i;
+
+	for (i = 0; (s[i] != c) && (s[i] != '\0'); i++)
+		;
+	if (s[i] == c)
+		return (s + i);
+	else
+		return ("nil");
 }

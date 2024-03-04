1c01916b42afcfbb19e8768ada0f3174e5e36358
essioui <salahessioui@gmail.com>
2024-03-04 10:42:31 +0100

    fills

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * *_memset - ills memory with a constant byte
+ * *_memset - fills memory with a constant byte
  * @s: pointer
  * @b: char
  * @n: integer
@@ -10,10 +10,10 @@ char *_memset(char *s, char b, unsigned int n)
 {
 unsigned i = 0;
 char p = *s;
-while (i < b)
+while (i < n)
 {
 *s++ = b;
 i++;
 }
-return (p);
+return ('p');
 }

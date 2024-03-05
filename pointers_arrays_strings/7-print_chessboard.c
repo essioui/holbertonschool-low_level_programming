6b16986706f0b0469e8fb508683c914b19aadfab
essioui <salahessioui@gmail.com>
2024-03-05 11:58:13 +0100

    chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*void print_chessboard - function print chessboard
+*@a: string
+*@8: entier 
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	i = j = 0;
+	while (i < 8)
+	{
+		j = 0;
+		while (j < 8)
+		{
+			_putchar(a[i][j]);
+			j++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}

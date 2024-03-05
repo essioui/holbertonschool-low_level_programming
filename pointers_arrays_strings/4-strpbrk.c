2913992c89bdcbc6b3e96c476812143d66b5c498
essioui <salahessioui@gmail.com>
2024-03-05 10:07:39 +0100

    strpbrk

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * _strpbrk - locates the first occurrence in the string
+ * @s: string
+ * @accept: string
+ * Return: null if not found else (s + i) 
+*/
+char *_strpbrk(char *s, char *accept)
+{
+unsigned int i, j;
+for (i = 0; *(s + i); i++)
+{
+    for (j = 0; *(accept + j); j++)
+    {
+        if (*(s + i) == *(accept + j))
+        {
+            break;
+        }
+    }
+    if (*(accept + j) != '\0')
+    {
+        return (s + i);
+    }
+}
+return ("nil");
+}

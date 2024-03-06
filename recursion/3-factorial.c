886a86cc7f6ad3cd84b34e3f2584e719fe4d3d29
essioui <salahessioui@gmail.com>
2024-03-06 12:52:01 +0100

    factorial

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *actorial - calcul n number with factorial
+ *factorial - calcul n number with factorial
  *@n: integer
  *Return: 0 if under or -1 if egual zero or number factorial
 */

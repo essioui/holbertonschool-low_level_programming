0cd167792b56e365e2083cb5e3c00faa1afeb27b
essioui <salahessioui@gmail.com>
2024-03-06 10:42:18 +0100

    "reverse"

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *_print_rev_recursion - function for print reverse string
+ *@s: string 
+*/
+void _print_rev_recursion(char *s)
+{
+if (s[0] != '\0')
+{
+_print_rev_recursion(s + 1);
+_putchar(s[0]);
+}
+else
+_putchar('\n');
+}

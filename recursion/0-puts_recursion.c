d10373167571f66099026ad3d58a6342b80a26ad
essioui <salahessioui@gmail.com>
2024-03-06 10:29:57 +0100

    "puts"

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * _puts_recursion - function puts recursion
+ *_puts_recursion - function puts recursion
  * 
- * @s: string
+ *@s: string
 */
 void _puts_recursion(char *s)
 {

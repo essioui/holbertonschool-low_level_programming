c28516c3c8bdb2b596c7865af990af80428e27dc
essioui <salahessioui@gmail.com>
2024-03-07 12:57:15 +0100

    prime_number

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * other - second function for output number
+ * @n: integer
+ * @i: integer
+ * @j: integer
+ * Return: 1 or 0
+*/
+
+
+int other(int n, int i, int j)
+{
+if ((n % i == 0 && i <= j) || n < 0 || n == 1)
+return (0);
+else if (n % i != 0 && i <= j)
+return (other(n, i + 1, j));
+else
+return (1);
+}
+
+/**
+ *is_prime_number - function for prime number
+ @n: integer
+ Return: the number prime
+*/
+int is_prime_number(int n)
+{
+return (other(n, 2, n / 2));
+}

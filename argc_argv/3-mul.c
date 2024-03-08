f17eedb32e16d43a61d58eb3af637fc033427e58
essioui <salahessioui@gmail.com>
2024-03-08 12:20:01 +0100

    mulitipli

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 /**
  *main - print the result of the multiplication
  *@argc: length
  *@argv: string
- *Return: 0 or 1 
+ *Return: 0 or 1
 */
 int main(int argc, char *argv[])
 {
-int result;
-if (argc < 2)
+int i;
+int j;
+if (argc < 3)
 {
-printf("Error \n");
+printf("%s\n", "Error");
 return (1);
 }
-result = *argv[1] * *argv[2];
-printf("%d\n", result);
-return (0);   
+i = atoi(argv[1]); /*function transfer from ACCII to integer*/
+j = atoi(argv[2]); /*function transfer from ASCII to integer*/
+printf("%d\n", i * j);
+return (0);
 }

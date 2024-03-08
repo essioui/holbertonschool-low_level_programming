bfab40bf068b5e063f4b3941a437288c7f94037b
essioui <salahessioui@gmail.com>
2024-03-08 13:44:39 +0100

    try_isdigit_with_it

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,23 +2,26 @@
 #include <stdlib.h>
 #include "main.h"
 /**
- * 
+ *main - adds positive numbers
+ *@argc: length string
+ *@arcv: string
+ *Return: 0 or 1
 */
 int main(int argc, char *argv[])
 {
-    int i, j, add = 0;
-    for (i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if(argv[i][j] > '9' || argv[i][j] < '0')
-            {
-            printf("Error\n");
-            return (1);
-            }
-        }
-        add = add + atoi(argv[i]);    
-    }
-    printf("%d\n", add);
-    return (0);
+int i, j, add = 0;
+for (i = 1; i < argc; i++)
+{
+for (j = 0; argv[i][j] != '\0'; j++)
+{
+if(argv[i][j] > '9' || argv[i][j] < '0')
+{
+printf("Error\n");
+return (1);
+}
+}
+add = add + atoi(argv[i]);    
+}
+printf("%d\n", add);
+return (0);
 }

12a6d5f9ba40270de1d7df1ab2996c3b9790bac9
essioui <salahessioui@gmail.com>
2024-03-08 10:16:49 +0100

    program_name

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * 
+ *main - give parameters to programs
+ *@argc: length of string
+ *@argv: string
+ *Return: 0
 */
 int main(int argc, char *argv[])
 {
-    if (argc > 0)
-        printf("%s\n", argv[0]);
-    return (0);
-    
+if (argc > 0)
+printf("%s\n", argv[0]);
+return (0);    
 }

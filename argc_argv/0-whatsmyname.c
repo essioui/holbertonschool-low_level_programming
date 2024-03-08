4bc137ecb68c2851326a135724b966b470854750
essioui <salahessioui@gmail.com>
2024-03-08 10:12:22 +0100

    program_name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * 
+*/
+int main(int argc, char *argv[])
+{
+    if (argc > 0)
+        printf("the program's name: %s\n", argv[0]);
+    return (0);
+    
+}

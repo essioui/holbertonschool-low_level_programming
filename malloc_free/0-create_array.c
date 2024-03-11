daecc115a2c8eae8493acd56cd27e121c3e66508
essioui <salahessioui@gmail.com>
2024-03-11 11:09:20 +0100

    create

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -1,5 +1,11 @@
 #include <stdlib.h>
 #include "main.h"
+/**
+ * *create_array - function for create array
+ * @size: integer number for length c
+ * @c: array
+ * Return: ar or null
+*/
 char *create_array(unsigned int size, char c)
 {
 char *ar;
@@ -11,7 +17,7 @@ return ('\0');
 }
 for (i = 0; i < size; i++)
 {
-    ar[i] = c;
+ar[i] = c;
 }
 return (ar);
 }

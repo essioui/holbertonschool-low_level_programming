d9773bf3a30e8d959a0b657117c3c3fd7bce6fc7
essioui <salahessioui@gmail.com>
2024-03-12 13:23:58 +0100

    allocate

---
+++ more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * *malloc_checked - function allocates memory used
+ * @b: integer
+ * Return: x
+*/
+void *malloc_checked(unsigned int b)
+{
+void *x;
+x = malloc(sizeof(b));
+if (x == NULL)
+exit(98);
+return (x);
+}

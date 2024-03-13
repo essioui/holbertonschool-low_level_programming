806a33e525f10de2afa6343711f0a90fe0fcd7a5
essioui <salahessioui@gmail.com>
2024-03-13 13:30:17 +0100

    concatenate

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -7,12 +7,12 @@
 */
 int _strlen(char *s)
 {
-    int length = 0;
-    while (s[length])
-    {
-        length++;
-    }
-    return (length);
+int length = 0;
+while (s[length])
+{
+length++;
+}
+return (length);
 }
 
 /**
@@ -24,34 +24,33 @@ int _strlen(char *s)
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    char *s;
-    unsigned int length1, length2, i, j, length;
-    if (s1 == NULL)
-    s1 = "";
-    if (s2 == NULL)
-    s2 = "";
-    length1 = _strlen(s1);
-    length2 = _strlen(s2);
-    if (n >= (unsigned int)length2)
-    {
-        length = length1 + length2;
-    }
-    else
-    length = length1 + n;
-
-    s = malloc((length + 1) * sizeof(char));
-    if (s == NULL)
-    return NULL;
-    else{
-    for (i = 0; i < length1; i++)
-    {
-        s[i] = s1[i];
-    }
-    for (j = 0; j < n && s2[j] != '\0'; j++)
-    {
-        s[i + j] = s2[j];
-    }
-    s[i + j] = '\0';
-    return (s);
-    }
+char *s;
+unsigned int length1, length2, i, j, length;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+length1 = _strlen(s1);
+length2 = _strlen(s2);
+if (n >= (unsigned int)length2)
+{
+length = length1 + length2;
+}
+else
+length = length1 + n;
+s = malloc((length + 1) * sizeof(char));
+if (s == NULL)
+return NULL;
+else{
+for (i = 0; i < length1; i++)
+{
+s[i] = s1[i];
+}
+for (j = 0; j < n && s2[j] != '\0'; j++)
+{
+s[i + j] = s2[j];
+}
+s[i + j] = '\0';
+return (s);
+}
 }

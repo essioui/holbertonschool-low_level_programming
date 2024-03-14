b91ee7b6b8f39c3c9d059386302299dcdbf2deb5
essioui <salahessioui@gmail.com>
2024-03-14 09:42:53 +0100

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include "main.h"
-/* *_calloc - fuction allocate in the memory for arrays
+/** 
+* *_calloc - fuction allocate in the memory for arrays
  * @nmemb: integer
  * @size: integer
  * Return: arrays or null
@@ -9,20 +10,20 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 char *pointers;
 unsigned int i;
-    if (nmemb == 0 || size == 0)
-    return NULL;
-
-    pointers = malloc(nmemb * size);
-    if (pointers == NULL)
-    {
-        return (NULL);
-    }
-    
-    i = 0;
-    while (i < nmemb * size)
-    {
-        pointers[i] = 0;
-        i++;
-    }
-    return (pointers);
+if (nmemb == 0 || size == 0)
+{
+return NULL;
+}
+pointers = malloc(nmemb * size);
+if (pointers == NULL)
+{
+return (NULL);
+}    
+i = 0;
+while (i < nmemb * size)
+{
+pointers[i] = 0;
+i++;
+}
+return (pointers);
 }

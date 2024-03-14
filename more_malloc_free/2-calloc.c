21a1d6fc1538615ee085e07862d036958345a932
essioui <salahessioui@gmail.com>
2024-03-14 09:37:41 +0100

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * 
+*/
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+char *pointers;
+unsigned int i;
+    if (nmemb == 0 || size == 0)
+    return NULL;
+
+    pointers = malloc(nmemb * size);
+    if (pointers == NULL)
+    {
+        return (NULL);
+    }
+    
+    i = 0;
+    while (i < nmemb * size)
+    {
+        pointers[i] = 0;
+        i++;
+    }
+    return (pointers);
+}

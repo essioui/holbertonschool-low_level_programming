7e1a05935c98e2366bc55826d96df0c09856a18a
essioui <salahessioui@gmail.com>
2024-03-15 10:14:47 +0100

    dog_h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,13 +1,15 @@
-#include <stdio.h>
-struct dog 
+#ifndef dog
+#define dog
+/**
+ * struct dog - structures
+ * @name: charactere
+ * @age: float
+ * @owner: char
+*/
+struct dog
 {
 char *name;
 float age;
 char *owner;
-};
-int main()
-{
-struct dog my_dog; 
-return (0);
-}
-    
+} my_dog;
+#endif

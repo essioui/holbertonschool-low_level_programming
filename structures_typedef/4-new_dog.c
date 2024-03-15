7cacb8929a7a7b833f21ed1faba1fa261e078221
essioui <salahessioui@gmail.com>
2024-03-15 13:56:27 +0100

    new_dog

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - entry point
+ * @name: string
+ * @age: number
+ * @owner: string
+ * Return: p
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+dog_t *p;
+p = malloc(sizeof(dog_t));
+if (p == NULL)
+return (NULL);
+if (name == NULL)
+{
+free(p);
+free(owner);
+return (NULL);
+}
+if (owner == NULL)
+{
+free(p);
+free(name);
+return (NULL);
+}
+p->name = name;
+p->age = age;
+p->owner = owner;
+return (p);
+}

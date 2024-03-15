e9774aff1273db6ffd75a2bb50d3d590bd806270
essioui <salahessioui@gmail.com>
2024-03-15 08:36:57 +0100

    24-hours

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /*
- * jack_bauer: 8-24_hours.c
- * Return: 0.
+ * jack_bauer - Prints every minute of the day of Jack Bauer
+ *
+ * Return: ...
  */
 void jack_bauer(void)
 {
---
+++ structures_typedef/dog.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+struct dog 
+{
+char *name;
+float age;
+char *owner;
+};
+int main()
+{
+struct dog my_dog; 
+return (0);
+}
+    

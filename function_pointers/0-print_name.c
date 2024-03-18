be015ef947b18494f4f6ae125465105056fe92ed
essioui <salahessioui@gmail.com>
2024-03-18 10:49:56 +0100

    print

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
 /**
  * print_name - function have function to pionter and pointer
- * @*name: char
- * @*f(): function to pointer
+ * @name: char
+ * @f: function to pointer
 */
 void print_name(char *name, void (*f)(char *))
 {

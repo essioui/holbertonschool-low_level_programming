c59ab07ce712a7f04801150ff61eb1c09cab4b40
essioui <salahessioui@gmail.com>
2024-03-18 12:29:01 +0100

    int_index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ * int_index - function
+ * @array: pointer to an integer array
+ * @size: size of the array
+ * @cmp: pointer to a function 
+ * Return: 0 or index of arrays
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    int i;
+    if (size <= 0 || array == NULL || cmp == NULL)
+    return (-1);
+
+    for (i = 0; i < size; i++)
+    {
+        if (cmp != 0)
+        {
+            return (i);
+        }
+        
+    }
+    
+   return (-1);
+}

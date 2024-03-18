8c91e3c916e8ba9c0cd553abebe87a5a6f707e1b
essioui <salahessioui@gmail.com>
2024-03-18 13:13:40 +0100

    int_index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,16 +8,14 @@
 */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-    int i;
-    if (size <= 0 || array == NULL || cmp == NULL)
-    return (-1);
+int i;
+if (size <= 0 || array == NULL || cmp == NULL)
+return (-1);
 
-    for (i = 0; i < size; i++)
-    {
-        if (cmp(array[i])) 
-            return (i);
-        
-    }
-    
-   return (-1);
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (i);
+}
+return (-1);
 }

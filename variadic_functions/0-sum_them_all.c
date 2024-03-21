920b8f5df6bb2e2c2e38ac1037fce24b0fb1e2aa
essioui <salahessioui@gmail.com>
2024-03-21 13:44:35 +0100

    0-sum variadic

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -5,15 +5,18 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
+
 unsigned int i;
 int sum;
+va_list count;
 
 if (n == 0)
 return(0);     
 
-va_list count;
 
-va_start(count, int);
+
+va_start(count, n);
+
 for (i = 0; i < n; i++)
 {
 sum += va_arg(count, int);

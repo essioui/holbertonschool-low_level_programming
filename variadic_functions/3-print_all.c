f3ed5e1bbd02abc107f44d6b4f4f3f33e696e0ad
essioui <salahessioui@gmail.com>
2024-03-22 13:14:27 +0100

    PRINT ALL

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -1,6 +1,5 @@
 #include "variadic_functions.h"
 
-
 /**
  * print_char - Prints a char
  * @ap: Argument pointer
@@ -10,16 +9,14 @@ void print_char(va_list ap)
 	printf("%c", va_arg(ap, int));
 }
 
-
 /**
- * print_int - Prints a integer
+ * print_integer - Prints an integer
  * @ap: Argument pointer
  */
-void print_int(va_list ap)
+void print_integer(va_list ap)
 {
 	printf("%d", va_arg(ap, int));
 }
-
 
 /**
  * print_float - Prints a float
@@ -27,9 +24,8 @@ void print_int(va_list ap)
  */
 void print_float(va_list ap)
 {
-	printf("%f", va_arg(ap, int));
+	printf("%f", va_arg(ap, double));
 }
-
 
 /**
  * print_string - Prints a string
@@ -37,46 +33,50 @@ void print_float(va_list ap)
  */
 void print_string(va_list ap)
 {
-	printf("%s", va_arg(ap, int));
-}
+	char *s = va_arg(ap, char *);
 
+	if (!s)
+	{
+		printf("(nil)");
+		return;
+	}
+		printf("%s", s);
+}
 
 /**
- * print_all - print all anything
- * @format: type of args
-*/
+ * print_all - Prints anything
+ * @format: Types of arguments passed to function
+ */
 void print_all(const char * const format, ...)
 {
+	print_type types[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list ap;
+	char *separator = "";
+	int i = 0;
+	int j = 0;
 
-print_type types[] = {
-{"c", print_char},
-{"i", print_int},
-{"f", print_float},
-{"s", print_string},
-{NULL, NULL}
-};
-va_list ap;
-char *separator = "";
-int i = 0;
-int j = 0;
-
-va_start(ap, format);
-while (format && format[i])
-{
-while (types[j].type)
-{
-if (*types[j].type == format[i])
-{
-printf("%s", separator);
-types[j].f(ap);
-separator = ", ";
-}
-++j;
-}
-j = 0;
-++i;
-}
-printf("\n");
-va_end(ap);
-
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		while (types[j].type)
+		{
+			if (*types[j].type == format[i])
+			{
+				printf("%s", separator);
+				types[j].f(ap);
+				separator = ", ";
+			}
+			++j;
+		}
+		j = 0;
+		++i;
+	}
+	printf("\n");
+	va_end(ap);
 }

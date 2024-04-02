80b37c4c4cb43d89ba07ff5be0adaad76ca55e87
essioui <salahessioui@gmail.com>
2024-04-02 10:29:23 +0100

    duplicated wit strup

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,24 +7,31 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-    list_t *new_node;
-    int i;
-    for (i = 0; str[i] != '\0'; i++)
-    ;
-    new_node = malloc(sizeof(size_t));
-    if (new_node = NULL)
-    {
-        return (NULL);
-    }
-    if (new_node-> str == NULL)
-    {
-        free(new_node);
-        return (NULL);
-    }
-    new_node->len = i;
-    new_node->next = *head;
+	int i, len;
+	char *content;
+	list_t *new;
 
-    *head = new_node;
-    return (head);
+	if (str == NULL || head == NULL)
+		return (NULL);
+	for (len = 0;str[len] != '\0'; len++)
+    ;
     
+    
+	new = *head;
+	content = malloc((len + 1) * sizeof(char));
+	if (content == NULL)
+		return (NULL);
+	for (i = 0; str[i]; i++)
+		content[i] = str[i];
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+	{
+		free(content);
+		return (NULL);
+	}
+	new->str = content;
+	new->len = len;
+	new->next = *head;
+	*head = new;
+	return (new);
 }

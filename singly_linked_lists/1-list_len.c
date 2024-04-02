adedfefd830c02df50b127081a9941d1607b7df8
essioui <salahessioui@gmail.com>
2024-04-02 09:27:34 +0100

    print_lenght_linked

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -6,11 +6,11 @@
 */
 size_t list_len(const list_t *h)
 {
-    size_t i = 0;
-    while (h)
-    {
-        i++;
-        h = h->next;
-    }
-    return (i);
+size_t i = 0;
+while (h)
+{
+i++;
+h = h->next;
+}
+return (i);
 }

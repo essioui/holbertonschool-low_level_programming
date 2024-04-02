3f91c00f6c7c78e49a472015e65289b37135aed9
essioui <salahessioui@gmail.com>
2024-04-02 09:07:52 +0100

    print_linked

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -6,15 +6,16 @@
 */
 size_t print_list(const list_t *h)
 {
-    size_t i = 0;
-    while (h != NULL)
-    {
-        if (h->str == NULL)
-            printf("[o] (nil)\n");
-            else
-            printf("[%u] %s\n", h->len, h->str);
-            h = h->next;
-            i++;  
-    }
-    return (i);
+size_t i = 0;
+
+while (h)
+{
+if (h->str)
+printf("[%u] %s\n", h->len, h->str);
+else
+printf("[0] (nil)\n");
+i++;
+h = h->next;
+}
+return (i);
 }

f367e739ec89a6a436b4b404c25ba336871c1744
essioui <salahessioui@gmail.com>
2024-04-02 10:55:42 +0100

    add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -7,31 +7,29 @@
 */
 list_t *add_node(list_t **head, const char *str)
 {
-	int i, len;
-	char *content;
-	list_t *new;
+int i, len;
+char *content;
+list_t *new;
 
-	if (str == NULL || head == NULL)
-		return (NULL);
-	for (len = 0;str[len] != '\0'; len++)
-    ;
-    
-    
-	new = *head;
-	content = malloc((len + 1) * sizeof(char));
-	if (content == NULL)
-		return (NULL);
-	for (i = 0; str[i]; i++)
-		content[i] = str[i];
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-	{
-		free(content);
-		return (NULL);
-	}
-	new->str = content;
-	new->len = len;
-	new->next = *head;
-	*head = new;
-	return (new);
+if (str == NULL || head == NULL)
+return (NULL);
+for (len = 0; str[len] != '\0'; len++)
+;
+new = *head;
+content = malloc((len + 1) * sizeof(char));
+if (content == NULL)
+return (NULL);
+for (i = 0; str[i]; i++)
+content[i] = str[i];
+new = malloc(sizeof(list_t));
+if (new == NULL)
+{
+free(content);
+return (NULL);
+}
+new->str = content;
+new->len = len;
+new->next = *head;
+*head = new;
+return (new);
 }

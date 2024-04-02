b369819a8b863aaf22551c7a2cc9c0232d3edb2b
essioui <salahessioui@gmail.com>
2024-04-02 13:36:24 +0100

    free_dlists

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+/**
+ * free_dlistint - function for free a dlistint_t list.
+ * @h: pointer of linked lists
+*/
+void free_dlistint(dlistint_t *head)
+{
+dlistint_t *cursor = head;
+while (head)
+{
+head = head->next;
+free(cursor);
+cursor = head;
+}
+}

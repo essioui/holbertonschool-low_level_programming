3a070e64a484acb2738e5dbb7a7f18af0ffa34fc
essioui <salahessioui@gmail.com>
2024-04-02 09:02:18 +0100

    print_all_lists

---
+++ singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - function prints all the elements of a list_t list.
+ * @h: pointer of the list
+ * Return: number of node
+*/
+size_t print_list(const list_t *h)
+{
+    unsigned int i = 0;
+    while (h != NULL)
+    {
+        if (h->str == NULL)
+            printf("[o] (nil)\n");
+            else
+            printf("[%u] %s\n", h->len, h->str);
+            h = h->next;
+            i++;  
+    }
+    return (i);
+}

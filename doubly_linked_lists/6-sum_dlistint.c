4690f3ffa64c5a5a3b5056c0cd6f2ad57d3d0994
essioui <salahessioui@gmail.com>
2024-04-03 10:49:01 +0100

    sum_lists

---
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * 
+*/
+int sum_dlistint(dlistint_t *head)
+{
+    int sum;
+    sum = 0;
+
+    if (head != NULL)
+    {
+        while (head != NULL)
+        {
+            sum += head->n;
+            head = head->next;
+        }
+        
+    }
+    return (sum);
+}

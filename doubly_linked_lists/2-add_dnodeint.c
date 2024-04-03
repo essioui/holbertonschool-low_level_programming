5769cf0cc102caa1ae6702f11ecba284aa0965eb
essioui <salahessioui@gmail.com>
2024-04-03 09:12:19 +0100

    add_dlists

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -4,30 +4,29 @@
 */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-    dlistint_t *new, *h;
-    new = malloc(sizeof(dlistint_t));
-    if (new = NULL)
-    {
-        return (NULL);
-    }
-    new->n = n;
-    new->prev = NULL;
-    h = *head;
+	dlistint_t *new;
+	dlistint_t *h;
 
-    if (h != NULL)
-    {
-        while (h->prev != NULL)
-        {
-            h = h->prev;
-        }
-    }
-    new->next = h;
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
-    if (h != NULL)
-    {
-        h->prev = new;    
-    }
-    *head = new;
-    
-    return (new);
+	new->n = n;
+	new->prev = NULL;
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
+	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
+
+	*head = new;
+
+	return (new);
 }

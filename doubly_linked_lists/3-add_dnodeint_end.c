078f05ca8c88f66c0028addef5848eb15017300b
essioui <salahessioui@gmail.com>
2024-04-03 09:46:34 +0100

    add_dlists_end

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -7,30 +7,29 @@
 */
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *h, *new;
+dlistint_t *h;
+dlistint_t *new;
 
-    new = malloc(sizeof(dlistint_t));
-    if (new == NULL)
-    return (NULL);
+new = malloc(sizeof(dlistint_t));
+if (new == NULL)
+return (NULL);
 
-    new->n = n;
-    new->next = NULL;
-    h = *head;
+new->n = n;
+new->next = NULL;
 
-    if (h != NULL)
-    {
-        while (h->next != NULL)
-        {
-            h = h->next;
-            h->next = new;
-        }
-    }
-    else {
-        *head = new;
-    }
+h = *head;
 
-    new->prev = h;
+if (h != NULL)
+{
+while (h->next != NULL)
+h = h->next;
+h->next = new;
+}
+else
+{
+*head = new;
+}
+new->prev = h;
 
-    return(new);
-    
+return (new);
 }

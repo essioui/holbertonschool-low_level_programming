a7948450baf04675736b2879e3df51b1a041317b
essioui <157033179+essioui@users.noreply.github.com>
2024-04-09 15:44:23 +0000

    append text to  end file

---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,42 @@
+#include "main.h"
+/**
+ * append_text_to_file - appends text to the end of a file
+ * @filename: path of the file
+ * @text_content: content to be appended
+ *
+ * Return: 1 on success -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+int file;
+ssize_t size, nb;
+
+if (filename == NULL)
+return (-1);
+
+file = open(filename, O_RDWR | O_APPEND);
+
+if (file < 0)
+return (-1);
+
+if (text_content == NULL)
+{
+close(file);
+return (1);
+}
+
+for (size = 0; text_content[size] != '\0'; size++)
+;
+
+nb = write(file, text_content, size);
+
+if (nb < 0)
+{
+close(file);
+return (-1);
+}
+close(file);
+return (1);
+
+}
